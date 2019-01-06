7c6076833965d54d4abb48dc1968f5313124e784
pandemnic <46409122+pandemnic@users.noreply.github.com>
2019-01-06 00:33:01 +0000

    Update reposter.js

--- reposter.js
+++ reposter.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("<SECRET_BOT_TOKEN>").catch(console.error);
+client.login("NTMxMjY1OTgxNTY2MTU2ODEz.DxLb-A.Kykntt1ZUjOzQ8e-PI2SZQA0BsE").catch(console.error);
 
 client.on("ready", function() {
 	client.user.setActivity(client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"), { type: "WATCHING" }).catch(console.error);
